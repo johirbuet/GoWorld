da5904042a30759d219b9c3bb4e146d913d89e8a
johirbuet <johirbuet@gmail.com>
2018-04-23 20:28:42 -0500

    code 2

---
+++ code2.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	var anInteger int = 42
+	var aString string = "Test"
+	str := "Hello"
+	fmt.Println(str)
+	fmt.Println(aString)
+	fmt.Println(anInteger)
+}
